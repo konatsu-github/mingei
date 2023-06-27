d4a11c086b7dd952958b4e0137a95b8b5929513f
konatsu-github <konatsu.business@gmail.com>
2023-06-27 22:28:06 +0900

    一旦

--- mingei/resources/views/components/ita.blade.php
+++ mingei/resources/views/components/ita.blade.php
@@ -1,7 +1,41 @@
-<div class="p-6 border-b border-gray-200 mb-4">
-    <h3>これはタイトルこれはタイトルこれはタイトルこれはタイトルこれはタイトルこれはタイトルこれはタイトルこれはタイトル</h3>
-    <hr>
-    <div>
-        <p>これはテストテストこれはテストテストこれはテストテストこれはテストテスト</p>
+<div class="bg-gray-300 p-6 border-b border-gray-200 mb-4 shadow-md">
+    <x-heading2>〇〇日のライブでこんな〇〇持っている方いませんか？</x-heading2>
+    <hr class="mb-4">
+    <div class="">
+        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+            <li class="py-3 sm:py-4">
+                <div class="flex items-center space-x-4">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="Neil image">
+                    </div>
+                    <div class="flex-1 min-w-0">
+                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            田中レモン
+                        </p>
+                        <p>どうも初めまして、そのコント道具にかんして家にありますのでもっていきますよ。どうも初めまして、そのコント道具にかんして家にありますのでもっていきますよ。どうも初めまして、そのコント道具にかんして家にありますのでもっていきますよ。</p>
+                        <time class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            2023/06/26 21:42
+                        </time>
+                    </div>
+
+                </div>
+            </li>
+            <li class="py-3 sm:py-4">
+                <div class="flex items-center space-x-4">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="Bonnie image">
+                    </div>
+                    <div class="flex-1 min-w-0">
+                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            田中レモン
+                        </p>
+                        <p>どうも初めまして、そのコント道具にかんして家にありますのでもっていきますよ。</p>
+                        <time class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            2023/06/26 21:42
+                        </time>
+                    </div>
+                </div>
+            </li>
+        </ul>
     </div>
 </div>
--- mingei/resources/views/dashboard.blade.php
+++ mingei/resources/views/dashboard.blade.php
@@ -3,40 +3,53 @@
         <h1 class="text-3xl font-bold tracking-tight text-gray-900">ダッシュボード</h1>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <x-content title="ライブ">
-                    <div class="mb-4">
-                        <x-heading2>前回のライブの平均評価</x-heading2>
-                        <x-rating rate='4' num='1' />
-                    </div>
-                    <div class="mb-4">
-                        <x-heading2>今までのライブの平均評価</x-heading2>
-                        <x-advanced-rating />
-                    </div>
-                </x-content>
-
-                <x-content title="コミュニティー">
-                    <div class="mb-4">
-                        <x-heading2>相方募集板</x-heading2>
-                        <x-ita />
-                    </div>
-                    <div class="mb-4">
-                        <x-heading2>コント道具貸し借り板</x-heading2>
-                        <x-ita />
-                    </div>
-                    <div class="mb-4">
-                        <x-heading2>ライブ代打探し板</x-heading2>
-                        <x-ita />
-                    </div>
-                    <div class="mb-4">
-                        <x-heading2>ネタの種板</x-heading2>
-                        <x-ita />
-                    </div>
-                </x-content>
+    <section class="bg-white dark:bg-gray-900">
+        <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+            <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">今までのライブの平均評価</h2>
+            </div>
+            <div class="grid grid-cols-2 gap-4 mt-8">
+                <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1">
+                <div>
+                    <x-advanced-rating />
+                </div>
 
             </div>
         </div>
-    </div>
+    </section>
+
+    <section class="bg-white dark:bg-gray-900">
+        <div class="items-center py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+            <x-heading1>コミュニティー</x-heading1>
+            <div class="mb-8">
+                <x-heading2>相方募集板</x-heading2>
+                <x-ita />
+            </div>
+            <div class="mb-8">
+                <x-heading2>コント道具貸し借り板</x-heading2>
+                <x-ita />
+            </div>
+            <div class="mb-8">
+                <x-heading2>ライブ代打探し板</x-heading2>
+                <x-ita />
+            </div>
+            <div class="mb-8">
+                <x-heading2>ネタの種板</x-heading2>
+                <x-ita />
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-white dark:bg-gray-900">
+        <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+            <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">今月の注目芸人</h2>
+                <p class="mb-4">今月の注目芸人はこの2組。新たな風を巻き起こす。バトル座で3連勝中で勢い満点！</p>
+            </div>
+            <div class="grid grid-cols-2 gap-4 mt-8">
+                <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1">
+                <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2">
+            </div>
+        </div>
+    </section>
 </x-app-layout>
