669db874c72ba1be9065a8457e86d2ea3243e97a
konatsu-github <konatsu.business@gmail.com>
2023-07-12 23:57:58 +0900

    一旦

--- mingei/resources/views/home.blade.php
+++ mingei/resources/views/home.blade.php
@@ -12,6 +12,9 @@
             <div class="grid max-w-lg gap-12 mx-auto mt-12 lg:grid-cols-3 lg:max-w-none">
 
                 <x-video-list-item src="/images/movieThumb/sample.jpg" alt="ネタ動画" publishedDays="3日前" views="2000" title="コント：お父さんの家" description="コントお父さんの家です" id="eagre32" profileLink="#" profileImage="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" name="田中レモン" combiName="有線電車" />
+                <x-video-list-item src="/images/movieThumb/sample.jpg" alt="ネタ動画" publishedDays="3日前" views="2000" title="コント：お父さんの家" description="コントお父さんの家です" id="eagre32" profileLink="#" profileImage="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" name="田中レモン" combiName="有線電車" />
+                <x-video-list-item src="/images/movieThumb/sample.jpg" alt="ネタ動画" publishedDays="3日前" views="2000" title="コント：お父さんの家" description="コントお父さんの家です" id="eagre32" profileLink="#" profileImage="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" name="田中レモン" combiName="有線電車" />
+                <x-video-list-item src="/images/movieThumb/sample.jpg" alt="ネタ動画" publishedDays="3日前" views="2000" title="コント：お父さんの家" description="コントお父さんの家です" id="eagre32" profileLink="#" profileImage="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" name="田中レモン" combiName="有線電車" />
 
 
             </div>
