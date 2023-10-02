ca6d2e78dbb7fa4972d3ed7e811b63a5ec91674d
konatsu-github <konatsu.business@gmail.com>
2023-10-02 22:10:55 +0900

    アップロード後にボタンにローダーを回す

--- mingei/resources/views/livewire/upload.blade.php
+++ mingei/resources/views/livewire/upload.blade.php
@@ -101,10 +101,11 @@
             </div>
         </div>
 
-        <div class="mt-6 flex items-center justify-end gap-x-6">
+        <div x-data="{ loadingButton: false }" class="mt-6 flex items-center justify-end gap-x-6">
             <button wire:click="resetFields" type="button" class="text-sm font-semibold leading-6 text-gray-900">キャンセル</button>
             @if ($thumbnail && $video && $title && $description)
-            <button type="submit" class="rounded-md bg-orange-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-400 cursor-pointer">アップロード</button>
+            <button x-on:click="loadingButton=true" x-show="!loadingButton" type="submit" class="rounded-md bg-orange-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-400 cursor-pointer">アップロード</button>
+            <div x-show="loadingButton" class="rounded-md bg-orange-400 px-3 py-2 shadow-sm"><img src="/images/common/loading.gif"></div>
             @else
             <div class="rounded-md bg-gray-400 px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-400">アップロード</div>
             @endif
